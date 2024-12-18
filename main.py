ebc13705be0e5a957301a9a7b90c1c8071485c92
EternalSyntaxError <146186588+EternalSyntaxError@users.noreply.github.com>
2024-12-18 16:55:00 -0500

    Commit for the 18th of December 2024. New camera intrinsic values after the diagonal FOV of the camera lens was accounted for during the latest calibration as of the 18th of December 2024.

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ from wpimath.geometry import Transform3d, Rotation3d, Pose3d, Translation3d, Coo
 aprilTagFieldLayout = robotpy_apriltag.AprilTagFieldLayout("TagPoses.json")
 
 # Load camera data
-Fx = 458.80  #Fx: x focal length
-Fy = 461.62  #Fy: y focal length
-Cx = 330.26  #Cx: x focal center (based on 640x480 resolution)
-Cy = 258.17  #Cy: y focal center (based on 640x480 resolution)
+Fx = 560.75
+Fy = 566.44
+Cx = 299.65
+Cy = 262.37
 
 poseEstimatorConfig = robotpy_apriltag.AprilTagPoseEstimator.Config(
 	0.1651,  #tag size in meters
@@ -25,8 +25,7 @@ poseEstimatorConfig = robotpy_apriltag.AprilTagPoseEstimator.Config(
 	Cy,
 )
 
-cameraDistortion = numpy.float32([ -0.077, 0.122, 0.002, 0.003, -0.121 ])
-
+cameraDistortion = numpy.float32([ 0.04, -0.101, 0.005, 0.001, 0.069 ])
 cameraIntrinsics = numpy.eye(3)
 cameraIntrinsics[0][0] = Fx
 cameraIntrinsics[1][1] = Fy
@@ -69,17 +68,16 @@ mat = numpy.zeros(shape=(xResolution, yResolution, 3), dtype=numpy.uint8)
 grayMat = numpy.zeros(shape=(xResolution, yResolution), dtype=numpy.uint8)
 
 # Colors for drawing
-lineColor = (0, 255, 0)
+lineColor = (0,255,0)
 
 # Position of the robot relative to the camera
-robotToCam = Transform3d(Translation3d(0, 0, 0),Rotation3d())
+robotToCam = Transform3d(Translation3d(0,0,0),Rotation3d())
 
 robotPos = Pose3d()
-
 # Main loop
 while True:
     robotPose = []
-    avgPose = (0, 0, 0)
+    avgPose = (0,0,0)
 
     _, mat = cvSink.grabFrame(mat)
     grayMat = cv2.cvtColor(mat, cv2.COLOR_RGB2GRAY)
@@ -107,20 +105,20 @@ while True:
                     distortedCorners[i][0] = corners[2 * i]
                     distortedCorners[i][1] = corners[2 * i + 1]
 
-                # Run the OpenCV undistortion routine to fix the corners
+                # run the OpenCV undistortion routine to fix the corners
                 undistortedCorners = cv2.undistortImagePoints(distortedCorners, cameraIntrinsics, cameraDistortion)
                 for i in range(4):
                     corners[2 * i] = undistortedCorners[i][0][0]
                     corners[2 * i + 1] = undistortedCorners[i][0][1]
 
-                # Run the pose estimator using the fixed corners
+                # run the pose estimator using the fixed corners
                 cameraToTag = poseEstimator.estimate(
                     homography = detection.getHomography(),
                     corners = tuple(corners))
                 tagID = detection.getId()
                 
                 if tagPose is not None:
-                    # First, we need to flip the Camera To Tag transform's angle 180 degrees around the y axis since the tag is oriented into the field
+                    # first we need to flip the Camera To Tag transform's angle 180 degrees around the y axis since the tag is oriented into the field
                     flipTagRotation = Rotation3d(axis = (0, 1, 0), angle = rotationsToRadians(0.5))
                     cameraToTag = Transform3d(cameraToTag.translation(), cameraToTag.rotation().rotateBy(flipTagRotation))
 
@@ -130,7 +128,7 @@ while True:
                     # We now have a corrected transform from the camera to the tag. Apply the inverse transform to the tag pose to get the camera's pose
                     cameraPose = tagPose.transformBy(cameraToTag.inverse())
 
-                    # Compute robot pose from robot to camera transform
+                    # compute robot pose from robot to camera transform
                     robotPose.append(cameraPose.transformBy(robotToCam.inverse()))
 
     # Set robotPos to the average position of all detections
@@ -138,9 +136,9 @@ while True:
         for pose in robotPose:
             avgPose = (avgPose[0] + pose.x, avgPose[1] + pose.y,avgPose[2] + pose.z)
         avgPose = (avgPose[0] / len(robotPose), avgPose[1] / len(robotPose), avgPose[2] / len(robotPose))
-        robotPos = Pose3d(Translation3d(avgPose[0], avgPose[1], avgPose[2]),Rotation3d())
+        robotPos = Pose3d(Translation3d(avgPose[0],avgPose[1],avgPose[2]),Rotation3d())
 
     # Publish everything
     outputStream.putFrame(mat)
-    robotCenter.set(list((round(robotPos.x, 6), round(robotPos.y, 6), -1 * round(robotPos.z, 6))))
+    robotCenter.set(list((round(robotPos.x,6),round(robotPos.y,6),-1 * round(robotPos.z,6))))
     aprilTagPresence.set(detections != [])
